9c9fd070cd3f27c4dd2deb6cd8554ce5309a5be7
Sagiring <102647163+Sagiring@users.noreply.github.com>
2023-06-29 18:22:27 +0800

    Client 6.29 18:22

--- Weq/client/client.py
+++ Weq/client/client.py
@@ -24,7 +24,8 @@ class Client:
     def send_msg(self, recv_ip, msg, recv_port=8888):
         key = self.session_key
         cipher = AES.new(key, AES.MODE_ECB)
-        msg = cipher.encrypt(pad(msg.encode('utf-8'), BLOCK_SIZE))
+        msg = base64.b64encode(msg.encode('utf-8'))
+        msg = cipher.encrypt(pad(msg, BLOCK_SIZE))
         print(base64.b64encode(msg))
         print(base64.b64encode(key))
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,11 +39,11 @@ class Client:
                 conn, addr = self.server.accept()
                 msg = base64.b64decode(conn.recv(8192))
                 key = self.session_key
-                print(base64.b64encode(msg))
-                print(base64.b64encode(key))
+                print(base64.b64decode(msg))
+                print(base64.b64decode(key))
                 cipher = AES.new(key, AES.MODE_ECB)
                 msg = unpad(cipher.decrypt(msg), BLOCK_SIZE).decode('utf-8', errors='ignore')
-                return msg
+                return base64.b64decode(msg)
                 # if msg == 'quit':
                 #     print('对方退出聊天')
                 #     conn.close()
