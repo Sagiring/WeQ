774d18cbe5e1c299e5dca3b3b5939124b1326413
Sagiring <102647163+Sagiring@users.noreply.github.com>
2023-06-30 10:23:03 +0800

    Client 6.30 10:23

--- Weq/client/client.py
+++ Weq/client/client.py
@@ -11,7 +11,7 @@ class Client:
     def __init__(self,session_key ,port = 8888):
         self.session_key = session_key
         self.server_port = port
-        self.recv_port = port
+        self.send_port = port
         addrs = socket.getaddrinfo(socket.gethostname(), None)
         for item in [addr[4][0] for addr in addrs]:
             if item[:2] == '10':
@@ -27,28 +27,31 @@ class Client:
                 time.sleep(0.1)
                 port += 1
         self.server.listen(5)
+        print(f'recv_port:{self.server_port}')
 
     def __del__(self):
         self.server.close()
 
     def send_msg(self, recv_ip, msg,isByte = False):
         key = self.session_key
+        self.send_port = 8888
         cipher = AES.new(key, AES.MODE_ECB)
         if isByte:
             msg = cipher.encrypt(pad(msg, BLOCK_SIZE))
         else:
             msg = cipher.encrypt(pad(msg.encode('utf-8'), BLOCK_SIZE))
         conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        port = self.recv_port
+        port = self.send_port
         while 1:
             try:
                 conn.connect((recv_ip,port))
-                self.recv_port = port
+                self.send_port = port
                 break
-            except OSError:
+            except ConnectionRefusedError:
                 time.sleep(0.1)
                 port += 1
         # print((str(len(msg))).encode())
+        print(f'send_port:{self.send_port}')
         conn.send((str(len(msg))).encode()+ b'\r\n\r\n' + msg)
         
 
